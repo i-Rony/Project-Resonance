c6c3aee0cff5ae7e5e37dd9d847a7ba7d5581976
i_Rony <ronrocks999@gmail.com>
2020-07-19 11:51:22 +0530

    Ron Commit 3

---
+++ screens/Login.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { View, Text, ImageBackground, TouchableOpacity } from 'react-native'
+
+const Login = ({navigation}) => {
+    return (
+        <ImageBackground 
+            source={require('C:/Users/Ron/Projects/resonance/assets/source.gif')} 
+            style={{
+                resizeMode: "cover", 
+                flex: 1,
+                zIndex: 0
+            }}
+        >
+            <View 
+                style={{
+                    zIndex: 1,
+                    flex: 1,
+                    justifyContent: "center",
+                    alignItems: "center",
+                }} 
+            >            
+                <TouchableOpacity 
+                    onPress={() => navigation.navigate('CreateAccount')}
+                    style={{
+                        justifyContent: "center",
+                        alignItems: "center",
+                        backgroundColor: "#0099FF",
+                        padding: 12,
+                        zIndex: 4,
+                        borderRadius: 30
+                    }}
+                >
+                    <Text style={{ fontWeight: 'bold', color: 'white' }}>Create Account</Text>
+                </TouchableOpacity>            
+            </View>
+        </ImageBackground>
+        
+    )
+}
+
+export default Login;
