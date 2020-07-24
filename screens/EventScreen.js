93705178e4ed988c8429cb6b019893a6be3ca3f4
i_Rony <ronrocks999@gmail.com>
2020-07-24 20:37:27 +0530

    Animated Fully Functional Tab Bar

---
+++ screens/EventScreen.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+
+import { useFonts } from '@use-expo/font';
+import { AppLoading } from 'expo';
+
+
+const EventScreen = ({navigation}) => {
+
+    let [fontsLoaded] = useFonts({
+		'Bold': require('../assets/fonts/Montserrat-Bold.ttf'),
+		'SemiBold': require('../assets/fonts/Montserrat-SemiBold.ttf'),
+		'Medium': require('../assets/fonts/Montserrat-Medium.ttf'),
+		'Light': require('../assets/fonts/Montserrat-Light.ttf'),
+		'Regular': require('../assets/fonts/Montserrat-Regular.ttf'),
+    });
+    
+    if (!fontsLoaded) {
+		return <AppLoading />;
+
+	} else {
+        return(
+            <View
+                style={{
+                    alignItems: "center",
+                    justifyContent: "center",
+                    flex: 1
+                }}
+            >
+                <TouchableOpacity
+                    style={{
+                        alignItems: "center",
+                        justifyContent: "center",
+                    }}                    
+                >
+                    <Text>
+                        Events
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+}
+
+export default EventScreen;
---
+++ screens/UserScreen.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+
+import { useFonts } from '@use-expo/font';
+import { AppLoading } from 'expo';
+
+
+const UserScreen = ({navigation}) => {
+
+    let [fontsLoaded] = useFonts({
+		'Bold': require('../assets/fonts/Montserrat-Bold.ttf'),
+		'SemiBold': require('../assets/fonts/Montserrat-SemiBold.ttf'),
+		'Medium': require('../assets/fonts/Montserrat-Medium.ttf'),
+		'Light': require('../assets/fonts/Montserrat-Light.ttf'),
+		'Regular': require('../assets/fonts/Montserrat-Regular.ttf'),
+    });
+    
+    if (!fontsLoaded) {
+		return <AppLoading />;
+
+	} else {
+        return(
+            <View
+                style={{
+                    alignItems: "center",
+                    justifyContent: "center",
+                    flex: 1
+                }}
+            >
+                <TouchableOpacity
+                    style={{
+                        alignItems: "center",
+                        justifyContent: "center",
+                    }}                    
+                >
+                    <Text>
+                        User
+                    </Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+}
+
+export default UserScreen;
