2bc90205edccabd2d36c91d968ec86523867e52e
i_Rony <ronrocks999@gmail.com>
2020-07-28 08:06:05 +0530

    Animated Posts Number

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -158,7 +158,6 @@ const ProfileScreen = ({ navigation }) => {
                             </Text>
                         </TouchableOpacity>
                     </View>
-
                 </LinearGradient>
             </ImageBackground>
         )
