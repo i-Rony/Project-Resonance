19a2d0ec22c5c6d529c7c7b1e542e28ef4e975a3
i_Rony <ronrocks999@gmail.com>
2020-07-31 06:19:41 +0530

    Follow UI changed

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { View, Text, ImageBackground, TouchableOpacity, Dimensions, TextInput} from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import AnimateNumber from 'react-native-animate-number';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 
 import { useFonts } from '@use-expo/font';
 import { AppLoading } from 'expo';
@@ -13,6 +15,16 @@ const ProfileScreen = ({ navigation }) => {
     const posts = 93;
 
     const bio = 'You thought it was Kawaii Chan, but it was actually me, DIO. Aboslute Anime-freak. People call me weeb'
+
+    const [isFollowing, setFollowing] = useState(false)
+
+    const togglefollow = () => {
+        if(isFollowing === true){
+            setFollowing(false);
+        } else {
+            setFollowing(true);
+        }
+    };
     
     let [fontsLoaded] = useFonts({
         'Bold': require('../assets/fonts/Montserrat-Bold.ttf'),
@@ -101,7 +113,9 @@ const ProfileScreen = ({ navigation }) => {
                             style={{
                                 fontFamily: 'Medium',
                                 fontSize: 30,
-                                color: 'white'
+                                color: 'white',
+                                flexDirection: 'column',
+                                flexWrap: 'wrap',
                             }}
                         >
                             Kawaii Chan
@@ -110,23 +124,46 @@ const ProfileScreen = ({ navigation }) => {
                             style={{
                                 fontFamily: 'Regular',
                                 fontSize: 22,
-                                color: 'white'
+                                color: 'white',
+                                flexDirection: 'column',
+                                flexWrap: 'wrap',
                             }}
                         >
                             Tokyo, Japan
                         </Text>
                         <TouchableOpacity                         
                             style={{ 
+                                flexDirection: 'column',
+                                flexWrap: 'wrap',
                                 left: width * 0.7, 
                                 padding: 12, 
                                 marginRight: -6, 
                                 paddingHorizontal: 20, 
                                 marginTop: -55, 
-                                borderColor: 'white', 
+                                borderColor: isFollowing === true ? 'rgba(231,90,124,1)' : 'white', 
                                 borderWidth: 1.5, 
-                                borderRadius: 12 
-                            }}                       
+                                borderRadius: 12,
+                                backgroundColor: isFollowing === true ? 'rgba(231,90,124,1)' : 'transparent'
+                            }}    
+                            onPress={() => togglefollow()}                   
                         >
+                            {isFollowing === true ? 
+
+                            <FontAwesomeIcon
+                                icon={faCheck}
+                                color='#2C363F'
+                                size={18}
+                                style={{
+                                    paddingHorizontal: 20,
+                                    padding: 10,
+                                    paddingRight: 26,
+                                    marginBottom: -1
+                                }}
+                            />
+                            
+                            
+                            : 
+
                             <Text
                                 style={{
                                     fontFamily: 'Regular',
@@ -134,7 +171,10 @@ const ProfileScreen = ({ navigation }) => {
                                 }}                                
                             >
                                 Follow
-                            </Text>
+                            </Text>                            
+
+                            }
+                            
                         </TouchableOpacity>
                     </View>
 
@@ -158,7 +198,6 @@ const ProfileScreen = ({ navigation }) => {
                         >
                             {bio}
                         </Text>
-
                     </View>
                 </LinearGradient>
             </ImageBackground>
