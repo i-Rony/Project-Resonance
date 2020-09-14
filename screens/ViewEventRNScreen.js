a47008027d6a1071352660d0299fb0089bb09f38
i-Rony <ronrocks999@gmail.com>
2020-09-14 22:09:41 +0530

    Join event added

--- screens/ViewEventRNScreen.js
+++ screens/ViewEventRNScreen.js
@@ -12,7 +12,7 @@ function ViewEventRNScreen({ route, navigation }) {
 
     return (
         <View style={{flex: 1, marginTop: Constants.statusBarHeight}}>
-            <View style={{flex: 0.1, elevation: 5, backgroundColor: 'white', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', paddingHorizontal: 12, paddingVertical: 2}}>
+            <View style={{flex: 0.12, elevation: 5, backgroundColor: 'white', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', paddingHorizontal: 12, paddingVertical: 2}}>
                 {/*Style 1*/}
                 <TouchableOpacity style={{marginHorizontal: -8, padding: 4}} onPress={() => navigation.goBack()}>
                     <FontAwesomeIcon
@@ -46,16 +46,16 @@ function ViewEventRNScreen({ route, navigation }) {
                 </TouchableOpacity> */}
             </View>
 
-            <View style={{flex: 0.4, elevation: 5, backgroundColor: 'white', paddingHorizontal: 12, borderBottomLeftRadius: 50, borderBottomRightRadius: 50}}>
-                <Text adjustsFontSizeToFit allowFontScaling numberOfLines={2} style={{fontFamily: 'Bold', fontSize: 26}}>
+            <View style={{flex: 0.44, elevation: 5, backgroundColor: 'white', paddingHorizontal: 12, borderBottomLeftRadius: 50, borderBottomRightRadius: 50}}>
+                <Text adjustsFontSizeToFit allowFontScaling numberOfLines={2} style={{flex:0.26, fontFamily: 'Bold', fontSize: 26}}>
                     {name}
                 </Text>
 
-                <Text adjustsFontSizeToFit allowFontScaling numberOfLines={6} style={{fontFamily: 'Regular', fontSize: 15, paddingVertical: 6, paddingBottom: 10}}>
+                <Text adjustsFontSizeToFit allowFontScaling numberOfLines={6} style={{flex:0.44,fontFamily: 'Regular', fontSize: 15, paddingVertical: 6, paddingBottom: 10}}>
                     {desc}
                 </Text>
 
-                <View style={{flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 12,alignItems: 'center', paddingTop: 12, borderTopWidth: 1, borderTopColor: 'rgba(0,0,0,0.1)'}}>
+                <View style={{flex:0.3, flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 12, alignItems: 'center', paddingTop: 0, borderTopWidth: 1, borderTopColor: 'rgba(0,0,0,0.1)'}}>
                     <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'space-evenly'}}>       
                         <FontAwesomeIcon 
                             icon={faClock}
@@ -104,6 +104,43 @@ function ViewEventRNScreen({ route, navigation }) {
                         </View>
                     </View>
                 </View>
+            </View>
+
+            <View style={{flex: 0.24, elevation: 4, top: -40, backgroundColor: 'white', paddingHorizontal: 12, borderBottomLeftRadius: 50, borderBottomRightRadius: 50, justifyContent: 'space-evenly'}}>                
+                <View style={{flex: 0.6, flexDirection: 'row', paddingTop: 36, alignItems: 'center' }}>
+                    <Image source={source} style={{resizeMode: 'cover', width: 44, height: 44, borderRadius: 10, marginHorizontal: 3}} />
+                    <Image source={source} style={{resizeMode: 'cover', width: 44, height: 44, borderRadius: 10, marginHorizontal: 3}} />
+                    <Image source={source} style={{resizeMode: 'cover', width: 44, height: 44, borderRadius: 10, marginHorizontal: 3}} />
+                    <Image source={source} style={{resizeMode: 'cover', width: 44, height: 44, borderRadius: 10, marginHorizontal: 3}} />
+                    <View style={{backgroundColor: 'pink', width: 44, height: 44, borderRadius: 10, alignItems: 'center', justifyContent: 'center', marginHorizontal: 3}}>
+                        <Text style={{fontFamily: 'Regular'}}>+4</Text>
+                    </View>
+                    <Text style={{fontFamily: 'Regular', marginHorizontal: 3}}>Members</Text>
+                </View>
+
+                {/* <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                    <TouchableOpacity style={{backgroundColor: 'pink', width: 44, height: 44, borderRadius: 10, alignItems: 'center', justifyContent: 'center', marginHorizontal: 3}}>
+                        <FontAwesomeIcon
+                            icon={faPlus}
+                            size={16}
+                            color='black'
+                        />
+                    </TouchableOpacity>
+                    
+                    <Text style={{fontFamily: 'Medium', marginHorizontal: 3}}>
+                        Join Event
+                    </Text>
+                </View> */}
+
+                <TouchableOpacity style={{flex: 0.4, backgroundColor: 'pink', marginHorizontal: -12, borderBottomLeftRadius: 50, borderBottomRightRadius: 50, alignItems: 'center', justifyContent: 'center'}}>
+                    <Text style={{fontFamily: 'SemiBold', fontSize: 16}}>
+                        Join Event
+                    </Text>
+                </TouchableOpacity>                
+            </View>
+
+            <View>
+
             </View>
         </View>
         // <View style={styles.viewEventContainer}>
