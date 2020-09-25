d3b6f5fd227669dab221bbd4ee0057c7d2bb095f
Ronit Chatterjee <ronrocks999@gmail.com>
2020-09-26 03:10:55 +0530

    Minor addition to band page

--- screens/BandScreen.js
+++ screens/BandScreen.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { View, Text, ImageBackground,TouchableOpacity } from 'react-native';
+import { View, Text, ImageBackground, TouchableOpacity, Dimensions } from 'react-native';
 import Constants from 'expo-constants';
+import { AppLoading } from 'expo';
 import Lofi from '../assets/lofi.jpg';
 import { LinearGradient } from 'expo-linear-gradient';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 import { useFonts } from '@use-expo/font';
+
+const {width, height} = Dimensions.get("screen");
 
 export default function BandScreen() {
 
@@ -16,6 +19,11 @@ export default function BandScreen() {
     'Light': require('../assets/fonts/Montserrat-Light.ttf'),
     'Regular': require('../assets/fonts/Montserrat-Regular.ttf'),
   });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+
+} else {
 
   return (
     <View>
@@ -47,13 +55,53 @@ export default function BandScreen() {
               </TouchableOpacity>              
             </View>
             
-            <Text numberOfLines={2} textBreakStrategy='balanced' allowFontScaling adjustsFontSizeToFit style={{position: 'absolute', bottom: 4, fontSize: 36, padding:8, fontFamily: 'SemiBold', color: 'black'}}>
+            <Text numberOfLines={2} textBreakStrategy='balanced' allowFontScaling adjustsFontSizeToFit style={{position: 'absolute', bottom: 4, fontSize: 36, padding:8, fontFamily: 'SemiBold', color: 'rgba(44, 54, 63, 0.834)'}}>
               Soundgarden
             </Text>            
           </LinearGradient>
         </ImageBackground>    
       </View>
 
+      <View>
+        <View style={{flexDirection: 'row', justifyContent: 'space-between', padding: 10, paddingTop: 14, alignItems: 'center'}}>
+          <Text
+            style={{
+              fontFamily: 'Medium',
+              fontSize: 20,
+              color: 'rgba(44, 54, 63, 0.834)'
+            }}
+          >
+            Tracks
+          </Text>
+          <TouchableOpacity
+            style={{                                       
+              borderRadius: 16,
+              backgroundColor: 'pink',
+            }}
+          >
+            <Text
+              style={{
+                fontFamily: 'Light',
+                color: 'rgba(44, 54, 63, 0.834)',
+                padding: 7
+              }}
+            >
+              Show All
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={{flexDirection: 'column'}}>
+            <View style={{flexDirection: 'row', justifyContent: 'space-between'}}><Text>Hello</Text><Text>Icons</Text></View>
+            <View style={{flexDirection: 'row', justifyContent: 'space-between'}}><Text>Hello</Text><Text>Icons</Text></View>
+            <View style={{flexDirection: 'row', justifyContent: 'space-between'}}><Text>Hello</Text><Text>Icons</Text></View>
+            <View style={{flexDirection: 'row', justifyContent: 'space-between'}}><Text>Hello</Text><Text>Icons</Text></View>
+            <View style={{flexDirection: 'row', justifyContent: 'space-between'}}><Text>Hello</Text><Text>Icons</Text></View>
+        </View>
+
+      </View>
+
      </View>
   );
+}
 }
